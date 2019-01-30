0d1bfb1cf872dc4a0140c49778e69ab15f6ea480
R Res <clonnes@hotmail.com>
2019-01-30 18:27:09 -0500

    Add project files.

---
+++ Assignment2_S19_Group_5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Assignment2_S19_Group_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
