afcd5e88738b9a43404c2a4b6d63762cce10bbd1
Mithra Sagar <mithrasagar@mail.usf.edu>
2019-02-04 13:22:02 -0500

    Exercises 1, 2, and 6 completed -Mithra

--- Assignment2_S19_Group_5/Program.cs
+++ Assignment2_S19_Group_5/Program.cs
@@ -48,88 +48,64 @@ namespace Assignment2_S19_Group_5
         // EXERCISE 3: Balanced sums
 
             Console.WriteLine("\n\nBalanced sums");
-
             List<int> arr = new List<int> { 1, 2, 3 };
-
             Console.WriteLine(balancedSums(arr));
+            Console.ReadKey(true);
 
 
-
-            // Missing numbers
+        // EXERCISE 4: Missing numbers
 
             Console.WriteLine("\n\nMissing numbers");
-
             int[] arr1 = { 203, 204, 205, 206, 207, 208, 203, 204, 205, 206 };
-
             int[] brr = { 203, 204, 204, 205, 206, 207, 205, 208, 203, 206, 205, 206, 204 };
-
             int[] r2 = missingNumbers(arr1, brr);
 
             displayArray(r2);
+            Console.ReadKey(true);
 
 
-
-            // grading students
+        // EXERCISE 5: grading students
 
             Console.WriteLine("\n\nGrading students");
-
             int[] grades = { 73, 67, 38, 33 };
-
             int[] r3 = gradingStudents(grades);
-
             displayArray(r3);
+            Console.ReadKey(true);
 
 
-
-            // find the median
+        // EXERCISE 6: find the median
 
             Console.WriteLine("\n\nFind the median");
-
             int[] arr2 = { 0, 1, 2, 4, 6, 5, 3 };
+            findMedian(arr2);
+            Console.ReadKey(true);
 
-            Console.WriteLine(findMedian(arr2));
-
-
-
-            // closest numbers
+        // EXERCISE 7: losest numbers
 
             Console.WriteLine("\n\nClosest numbers");
-
             int[] arr3 = { 5, 4, 3, 2 };
-
             int[] r4 = closestNumbers(arr3);
-
             displayArray(r4);
+            Console.ReadKey(true);
 
 
-
-            // Day of programmer
-
+        // EXERCISE 8: Day of programmer
             Console.WriteLine("\n\nDay of Programmer");
-
             int year = 2017;
-
             Console.WriteLine(dayOfProgrammer(year));
-
+            Console.ReadKey(true);
         }
 
 
 
         static void displayArray(int[] arr)
         {
-
             Console.WriteLine();
-
             foreach (int n in arr)
             {
-
                 Console.Write(n + " ");
-
             }
-
         }
-
-
 
         // Complete the rotLeft function below.
 
@@ -149,7 +125,7 @@ namespace Assignment2_S19_Group_5
             try
             {
                 // Establish variables that will be used in calculating
-                int min_position, temp;
+                int min_position2, temp2;
                 int numToys = 0, totPrice = 0;
 
                 // Ask user for their desired budget
@@ -163,23 +139,23 @@ namespace Assignment2_S19_Group_5
                 for (int i = 0; i < prices.Length; i++)
                 {
                     // Sets min_position to keep track of element with lowest value
-                    min_position = i;
+                    min_position2 = i;
                     // Begins to cycle through array
                     for (int x = i + 1; x < prices.Length; x++)
                     {
                         // If the next element is smaller, then change min_position
-                        if (prices[x] < prices[min_position])
+                        if (prices[x] < prices[min_position2])
                         {
-                            min_position = x;
+                            min_position2 = x;
                         }
                     } // End of inner for loop
                     // If min_position does not equal current element evaluated
                     // then perform the swap so that this is now the lowest element
-                    if (min_position != 1)
+                    if (min_position2 != 1)
                     {
-                        temp = prices[i];
-                        prices[i] = prices[min_position];
-                        prices[min_position] = temp;
+                        temp2 = prices[i];
+                        prices[i] = prices[min_position2];
+                        prices[min_position2] = temp2;
                     }
                     Console.Write(" " + prices[i]);
                 } // End of outer for loop
@@ -218,80 +194,95 @@ namespace Assignment2_S19_Group_5
             return 0;
         }
 
-
-
         // Complete the balancedSums function below.
 
         static string balancedSums(List<int> arr)
 
         {
-
             return "";
-
         }
-
-
 
         // Complete the missingNumbers function below.
 
         static int[] missingNumbers(int[] arr, int[] brr)
 
         {
-
             return new int[] { };
-
         }
-
-
-
-
 
         // Complete the gradingStudents function below.
 
         static int[] gradingStudents(int[] grades)
 
         {
-
             return new int[] { };
-
         }
-
-
-
+    
         // Complete the findMedian function below.
 
-        static int findMedian(int[] arr)
-
+        static int findMedian(int[] arr2)
         {
+            try
+            {
+                // Establish variables that will be used in calculating
+                int min_position6;
+                int temp;
 
+                Console.Write("The median of the following sorted array [");
+                // Perform selection sort to go through array and place in ascending order
+                for (int i = 0; i < arr2.Length; i++)
+                {
+                    // Sets min_position to keep track of element with lowest value
+                    min_position6 = i;
+                    // Begins to cycle through array
+                    for (int x = i + 1; x < arr2.Length; x++)
+                    {
+                        // If the next element is smaller, then change min_position
+                        if (arr2[x] < arr2[min_position6])
+                        {
+                            min_position6 = x;
+                        }
+                    } // End of inner for loop
+                    // If min_position does not equal current element evaluated
+                    // then perform the swap so that this is now the lowest element
+                    if (min_position6 != 1)
+                    {
+                        temp = arr2[i];
+                        arr2[i] = arr2[min_position6];
+                        arr2[min_position6] = temp;
+                    }
+                    Console.Write(" " + arr2[i]);
+                } // End of outer for loop
+
+                // Divide sorted length of array by 2 to gain median value
+                int med = arr2.Length / 2;
+                // Display median value in output
+                Console.Write(" ] is: " + med);
+            }
+            catch
+            {
+                // Catch block to handle exceptions
+                Console.WriteLine("\nThe program failed to calculate. Please close and try again.");
+                Console.ReadKey(true);
+            }
             return 0;
-
         }
-
-
 
         // Complete the closestNumbers function below.
 
         static int[] closestNumbers(int[] arr)
 
-        {
-
+        {       
             return new int[] { };
-
         }
-
-
 
         // Complete the dayOfProgrammer function below.
 
         static string dayOfProgrammer(int year)
 
         {
-
             return "";
-
         }
 
-    }
-
-}
+    } // End of class
+} // End of namespace
